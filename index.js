50467f539788743e9d9eda7af059e0a64fc7970d
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-11-22 15:59:29 -0500

    commit tree 6de9f227fdb3e5b9b09b247e33de9963f31a2ecd

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { expect } = require("chai");
+
 // Add your code here
+function submitData(firstName, email){
+    const confgObj = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+        };
+        return fetch("http://localhost:3000/users", confgObj)
+        .then(res => res.json())
+        .then(body => addNewContact(body.id))
+        .catch(error => document.body.innerHTML = error.message)
+          setTimeout(() => expect(document.innerHTML).to.include(message),500)
+        
+    }
+    function addNewContact(newID){
+        document.getElementsByTagName("body")[0].innerHTML = newID;
+        document.append(message)
+    }
+submitData("Name","name@name.com")
+let message = "Unauthorized Access"
